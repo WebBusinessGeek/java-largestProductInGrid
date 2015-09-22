053f803892d09dd22a6cd685cd2460addfee1abf
Kevin Washington <kevw12188@gmail.com>
2015-09-22 11:40:57 -0400

    still working

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.lang.reflect.Array;
 /**
  * Created by MacBookEr on 9/22/15.
  */
@@ -5,8 +7,21 @@ public class Algorithm {
 
     private String testsAreWorkingMessage = "Tests are working";
 
-    public String getTestsAReWorkingMessage() {
+    public String getTestsAreWorkingMessage() {
         return this.testsAreWorkingMessage;
+    }
+
+    public ArrayList <Integer> convertStringIntoGrid(String stringToConvert) {
+        ArrayList <Integer> result = new ArrayList<>();
+        String[] converted = stringToConvert.split(" ");
+        int counter = 0;
+        while(counter < converted.length) {
+            String frontOfInt = converted[counter][0];
+            int convertedToInt = Integer.parseInt(converted[counter].trim());
+            result.add(convertedToInt);
+            counter++;
+        }
+        return result;
     }
 }
 
