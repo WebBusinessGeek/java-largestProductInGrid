32553850a7d1c599b4c7d2dc7385ec1ec4554bcc
Kevin Washington <kevw12188@gmail.com>
2015-09-22 19:39:54 -0400

    added functionality to convert string into an integer array list

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,8 +1,5 @@
-import java.util.*;
-import java.lang.reflect.Array;
-/**
- * Created by MacBookEr on 9/22/15.
- */
+import java.util.ArrayList;
+
 public class Algorithm {
 
     private String testsAreWorkingMessage = "Tests are working";
@@ -11,16 +8,19 @@ public class Algorithm {
         return this.testsAreWorkingMessage;
     }
 
-    public String[] convertStringIntoIntegerArray(String stringToConvert) {
+    public ArrayList <Integer> convertStringIntoIntegerArray(String stringToConvert) {
+        ArrayList <Integer> convertedIntegers = new ArrayList<>();
         String[] breakUpString = stringToConvert.split("\\s+");
         int counter = 0;
         while(counter < breakUpString.length) {
             if(breakUpString[counter].substring(0,1) == "0") {
-                
+                 breakUpString[counter] = breakUpString[counter].substring(1);
             }
+            int convertToInt = Integer.parseInt(breakUpString[counter]);
+            convertedIntegers.add(convertToInt);
             counter++;
         }
-
+    return convertedIntegers;
     }
 }
 
